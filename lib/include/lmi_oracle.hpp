328f7eb9fb43b519b1a5bed62d19109957ae90e0
Live session user <luk036@gmail.com>
2018-03-29 17:21:36 +0800

    add lmi stuff

--- lib/include/lmi_oracle.hpp
+++ lib/include/lmi_oracle.hpp
@@ -2,39 +2,43 @@
 #define _HOME_UBUNTU_CUBSTORE_ELLCPP_LMI_ORACLE_HPP 1
 
 //#include "mat.hpp"
-#include "chol_ext.hpp"
 #include <xtensor/xarray.hpp>
 #include <xtensor-blas/xlinalg.hpp>
+#include "chol_ext.hpp"
 
+/**
+ * @brief  Oracle for Linear Matrix Inequality 
+ * 
+ * Oracle for:
+ *    F * x <= B
+ * or
+ *    (B - F * x) must be a semidefinte matrix
+ */
 class lmi_oracle {
-  using Vec = xt::xarray<double>;
-  using Mat = xt::xarray<double>;
   using Arr = xt::xarray<double>;
-  using shape_type = Vec::shape_type;
+  using shape_type = Arr::shape_type;
 
 private:
   Arr& _F;
+  Arr& _F0;
 
 public:
-  explicit lmi_oracle(Arr& F) : _F{F} {}
+  explicit lmi_oracle(Arr& F, Arr& B) : _F{F}, _F0{B} {}
 
-  auto chk_mtx(Mat &A, const Vec &x) const {
-    // auto sub_mat = [](const Mat &M, size_t p) {
-    //   return bnu::project(M, bnu::range(0, p), bnu::range(0, p));
-    // };
+  auto chk_mtx(Arr &A, const Arr &x) const {
     using xt::placeholders::_;
     using xt::linalg::dot;
 
     auto n = x.size();
-    auto g = Vec(n);
+    auto g = Arr(n);
     auto fj = -1.0;
     for (auto i = 0u; i < n; ++i) {
       auto Fi = xt::view(_F, xt::all(), xt::all(), i);
-      A += Fi * x[i];
+      A -= Fi * x[i];
     }
     chol_ext Q(A);
     if (Q.is_sd()) {
-      return std::make_tuple(g, fj);
+      return std::tuple{g, fj};
     }
     auto v = Q.witness();
     auto p = v.size();
@@ -42,26 +46,28 @@ public:
     fj = -dot(v, dot(xt::view(A, sub, sub), v))();
     for (auto i = 0u; i < n; ++i) {
       auto Fi = xt::view(_F, xt::all(), xt::all(), i);
-      g[i] = -dot(v, dot(xt::view(Fi, sub, sub), v))();
+      g[i] = dot(v, dot(xt::view(Fi, sub, sub), v))();
     }
-    return std::make_tuple(g, fj);
+    return std::tuple{g, fj};
   }
 
-  auto chk_spd_t(const Vec &x, double t) const {
-    Mat A = _F[x.size()];
-    auto m = A.size();
-    for (auto i = 0u; i < m; ++i) {
-      A(i, i) += t;
-    }
+  auto chk_spd_t(const Arr &x, double t) const {
+    Arr A = _F0;
+    // ???
+    // auto m = A.size();
+    // for (auto i = 0u; i < m; ++i) {
+    //   A(i, i) += t;
+    // }
+    A += t;
     return this->chk_mtx(A, x);
   }
 
-  auto chk_spd(const Vec &x) const {
-    Mat A = _F[x.size()];
+  auto chk_spd(const Arr &x) const {
+    Arr A = _F0;
     return this->chk_mtx(A, x);
   }
 
-  auto operator()(const Vec &x, double t) const {
+  auto operator()(const Arr &x, double t) const {
     auto [g, fj] = this->chk_spd_t(x, t);
     if (fj < 0.0) {
       t -= 1.0;
---
+++ lib/test/src/lmi_test.cpp
@@ -0,0 +1,85 @@
+/*
+ *  Distributed under the MIT License (See accompanying file /LICENSE )
+ */
+#include <catch.hpp>
+#include <ell.hpp>
+#include <cutting_plane.hpp>
+#include <lmi_oracle.hpp>
+#include <xtensor/xarray.hpp>
+#include <xtensor-blas/xlinalg.hpp>
+#include <tuple>
+
+// using namespace fun;
+class my_oracle {
+  using Arr = xt::xarray<double>;
+
+private:
+  Arr _c;
+  lmi_oracle _lmi1;
+  lmi_oracle _lmi2;
+
+private:
+  explicit my_oracle(Arr& c, Arr& F1, Arr&B1, Arr&F2, Arr& B2):
+    _c{c}, _lmi1{F1, B1}, _lmi2{F2, B2} {}
+
+	auto operator()(Arr& x, double t) const {
+    using xt::linalg::dot;
+
+		auto f0 = dot(_c, x)();
+		auto fj1 = f0 - t;
+		if (fj1 > 0.0) { 
+			return std::tuple{_c, fj1, t};
+    }
+
+		auto [g2, fj2] = _lmi1.chk_spd(x);
+		if (fj2 > 0.) {
+			return std::tuple{g2, fj2, t};
+    }
+		
+    auto [g3, fj3] = self.lmi2.chk_spd(x)
+		if (fj3 > 0.) {
+			return std::tuple{g3, fj3, t};
+    }
+
+		return std::tuple{_c, 0.0, f0}
+  }
+}
+
+
+TEST_CASE( "LMI test", "[lmi_oracle]" ) {
+  using Arr = xt::xarray<double>;
+
+	auto c =  Arr{1.,-1.,1.};
+  auto F1 = Arr({ {{-7., -11.}, {-11.,  3.}}, 
+                  {{ 7., -18.}, {-18., 8.}}, 
+                  {{-2.,  -8.}, {-8., 1.}} } );
+	auto B1 = Arr({{33.,-9.},{-9.,26.}});
+	auto F2 = Arr({ {{-21., -11.,   0.}, {-11.,  10.,   8.}, {  0.,   8.,  5.}},
+                  {{  0.,  10.,  16.}, { 10., -10., -10.}, { 16., -10.,  3.}},
+                  {{ -5.,   2., -17.}, {  2.,  -6.,   8.}, {-17.,   8.,  6.}} } );
+	auto B2 = Arr({{ 14.,   9.,  40.}, {  9.,  91.,  10.}, { 40.,  10., 15.}} );
+
+  auto P = my_oracle(c, F1, B1, F2, B2);
+  auto E = ell(100.0, Arr{0.0, 0.0, 0.0});
+
+  std::tie(std::ignore, fb, iter, flag, status) =
+        cutting_plane_dc(P, E, 100.0, 200, 1e-4);
+  // fmt::print("{:f} {} {} {} \n", fb, iter, flag, status);
+  std::cout << fb << ", " << iter << ", " << flag << ", " << status << "\n";
+
+  REQUIRE( flag == 1 );
+  REQUIRE( iter == 115 );
+}
+
+// TEST_CASE( "Projective Point", "[proj_plane]" ) {
+//     REQUIRE( l.incident({l, m}) );
+// }
+
+
+// int main(int argc, char* argv[]) {
+//   //using namespace ModernCppCI;
+
+//   auto result = Catch::Session().run(argc, argv);
+
+//   return (result < 0xff ? result : 0xff);
+// }
